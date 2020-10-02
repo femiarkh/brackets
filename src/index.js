1e9a1fd971037a500b32b3d9604166f5b3255d6b
Dmitry Gutkovsky <femiarkh@gmail.com>
2020-10-02 14:29:42 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    'use strict';    
+
+    const array = str.split('');
+    
+    let configIndex;   
+
+    for (let i = 0; i < Math.floor(array.length / 2); i++) {
+
+        bracketsConfig.forEach((item, index) => {
+            if (item[0] === array[i]) {
+                configIndex = index;                             
+            }            
+        }); 
+
+        if (!configIndex && configIndex !== 0) {
+            return false;
+        }
+        const closingIndex = array.indexOf(bracketsConfig[configIndex][1], i + 1);
+        if (closingIndex === -1) {
+            return false;
+        }
+        if (closingIndex === i + 1) {
+            array.splice(i, 2);
+            
+            return check(array.join(''), bracketsConfig);            
+        }
+    }       
+    
+    return array.length === 0 ? true : false;
 }
